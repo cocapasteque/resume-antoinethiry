186290ffbc7a3dc0775e37870aa05b22667769f8
cocapasteque <thiryantoine@gmail.com>
2024-09-24 15:48:03 +0200

    Home component: added logo and refined animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,32 @@ module.exports = {
             transform: "translate(0, 0)",
             opacity: 1
           }
+        },
+        "text-reveal-right" : {
+          "0%": {
+            transform: "translate(-100%, 0)",
+            opacity: 0
+          },
+          "50%": {
+            opacity: 0
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+            opacity: 1
+          }
+        },
+        "text-reveal-left" : {
+          "0%": {
+            transform: "translate(100%, 0)",
+            opacity: 0
+          },
+          "50%": {
+            opacity: 0
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+            opacity: 1
+          }
         }
       }
     },
